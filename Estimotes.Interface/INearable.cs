065281672e91576e16023316bfcb776f67d2d4e1
Allan Ritchie <allan.ritchie@gmail.com>
2015-07-27 16:12:42 -0400

    minor

--- Estimotes.Interface/INearable.cs
+++ Estimotes.Interface/INearable.cs
@@ -12,3 +12,17 @@
 //        int Rssi { get; }
 //    }
 //}
+//public enum NearableTypes {
+//	Unknown = 0,
+//	Dog = 1,
+//	Car = 2,
+//	Fridge = 3,
+//	Bag = 4,
+//	Bike = 5,
+//	Chair = 6,
+//	Bed = 7,
+//	Door = 8,
+//	Shoe = 9,
+//	Generic = 10,
+//	All = 11
+//}
